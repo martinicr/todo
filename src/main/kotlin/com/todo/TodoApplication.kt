bd83c30e237f8a5dc8285e8833d28657f986614c
Martin Flores <martin.flores@bodybuilding.com>
2018-05-18 22:03:08 -0600

    commit inicial

---
+++ src/main/kotlin/com/todo/TodoApplication.kt
@@ -0,0 +1,67 @@
+package com.todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.io.Serializable
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@SpringBootApplication
+class TodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoApplication>(*args)
+}
+
+
+@Entity
+data class Todo(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    val created: Date? = null,
+    val title: String? = null,
+    val summary: String? = null
+) : Serializable
+
+@Repository
+interface TodoRepository : JpaRepository<Todo, Long> {
+
+}
+
+@Service
+class TodoService(val todoRepository : TodoRepository) {
+
+    fun getTodos() : List<Todo> {
+        return this.todoRepository.findAll()
+    }
+
+    fun addTask(todo: Todo) {
+        this.todoRepository.save(todo)
+    }
+
+}
+
+@RestController
+@RequestMapping("/todos")
+class TodoController(val todoService : TodoService) {
+
+    @GetMapping("", "/")
+    fun allTodos() : List<Todo> {
+        return this.todoService.getTodos()
+    }
+
+    @PostMapping("", "/")
+    fun addTask(@RequestBody newTodo : Todo){
+        this.todoService.addTask(newTodo)
+    }
+
+}
---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,1 @@
+insert into todo(created, title, summary) values('2018-05-22', 'TODO', 'My TODO');
